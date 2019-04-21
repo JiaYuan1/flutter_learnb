778d5485dcdc8a92f542c9abbd9cf89bfdbf46f7
JiaYuan1 <j1305081045@163.com>
2019-04-21 16:01:41 +0800

    ninghao第一个flutter

---
+++ lib/model/post.dart
@@ -0,0 +1,33 @@
+class Post{
+
+const Post({this.title,this.author,this.imageUrl});
+
+final String title;
+final String author;
+final String imageUrl;
+
+}
+
+
+final List<Post> posts=[
+  Post(
+    title: 'Candy Shop',
+    author: 'Mohamed Chahin',
+    imageUrl:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1207259703,335941331&fm=200&gp=0.jpg', 
+  ),
+   Post(
+    title:'Candy Shop' ,
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1207259703,335941331&fm=200&gp=0.jpg', 
+  ),
+   Post(
+    title: 'Candy Shop',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1207259703,335941331&fm=200&gp=0.jpg', 
+  ),
+   Post(
+    title: 'Candy Shop',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1207259703,335941331&fm=200&gp=0.jpg', 
+  ),
+];
